c2ca20d6ac8c40f0cb438bdefcb19ec5f89bd33a
linlingiqn <linlingqin77@qq.com>
2025-12-11 13:41:54 +0800

    feat: 更新依赖安装命令，允许锁定文件更新，优化 Dockerfile 和工作流配置

--- ruoyi-plus-soybean/build/plugins/unocss.ts
+++ ruoyi-plus-soybean/build/plugins/unocss.ts
@@ -5,12 +5,12 @@ import presetIcons from '@unocss/preset-icons';
 import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders';
 
 export function setupUnocss(viteEnv: Env.ImportMeta) {
-  const { VITE_ICON_PREFIX, VITE_ICON_LOCAL_PREFIX } = viteEnv;
+  const { VITE_ICON_PREFIX = 'icon', VITE_ICON_LOCAL_PREFIX = 'icon-local' } = viteEnv;
 
   const localIconPath = path.join(process.cwd(), 'src/assets/svg-icon');
 
   /** The name of the local icon collection */
-  const collectionName = VITE_ICON_LOCAL_PREFIX.replace(`${VITE_ICON_PREFIX}-`, '');
+  const collectionName = VITE_ICON_LOCAL_PREFIX?.replace(`${VITE_ICON_PREFIX}-`, '') || 'local';
 
   return unocss({
     presets: [
